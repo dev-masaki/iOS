e478a67c8f765a070296bfccdf561b7eb7e0ad4d
dev-masaki <tubobobobow.dev@gmail.com>
2015-12-21 13:54:10 +0900

    _

--- ClassEx/ClassEx/Device.swift
+++ ClassEx/ClassEx/Device.swift
@@ -6,4 +6,27 @@
 //  Copyright © 2015年 Masaki Otsubo. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+// Deviceクラス
+class Device {
+    // プロパティの定義
+    var name: String = ""  // 名前
+    var version: Int = 0  // バージョン
+    
+    // メソッドの定義
+    func info() -> String {
+        return "\(name)のバージョンは\(version)"
+    }
+    
+    // 引数なしのイニシャライザ
+    convenience init() {
+        self.init(name: "none", version: 1)
+    }
+    
+    // 引数ありのイニシャライザ
+    init(name: String, version: Int) {
+        self.name = name
+        self.version = version
+    }
+}
